352266deab756be9001384c039ff4a25d09c87ff
Under Code . Org <50494184+undercodeorg@users.noreply.github.com>
2019-06-05 12:25:58 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -234,8 +234,9 @@ var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function(){
     var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+		current[0].classList.toggle('active');
+		// current[0].classList.remove('active');
+    this.classList.toggle('active')
   });
 }
 
